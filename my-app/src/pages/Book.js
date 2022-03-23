7b7ca0f0655e0d752cfa05d1ad01da0ed5eb6c30
Profana92 <87131514+Profana92@users.noreply.github.com>
2022-03-23 12:13:38 +0100

    restructure

--- my-app/src/pages/Book.js
+++ my-app/src/pages/Book.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
-import Header from "./partial/Header";
-import BookDescription from "./Book/BookDescription";
-import Footer from "./partial/Footer";
+import Header from "../components/partial/Header";
+import BookDescription from "../components/book/BookDescription";
+import Footer from "../components/partial/Footer";
 
 class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          title: "Hobbit",
+          author: "J.R.Tolkien",
+          pages: 455,
+        },
+        {
+          title: "Rok 1984",
+          author: "G.orwell",
+          pages: 455,
+        },
+        {
+          title: "Władca Pierścieni",
+          author: "J.R.Tolkien",
+          pages: 669,
+        },
+        {
+          title: "Stary człowiek i morze",
+          author: "E. Hemingway",
+          pages: 226,
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div>
         <Header />
-        <BookDescription />
+        {this.state.books.map((book, i) => (
+          <BookDescription
+            key={i}
+            book={book}
+            // deleteBook={this.deleteSingleBook}
+          />
+        ))}
+
         <Footer />
       </div>
     );
